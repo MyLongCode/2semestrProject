f2757a64730d89823a8f38a65a4b115ce303c486
Krabs <vovastr0v@yandex.ru>
2023-04-24 17:17:16 +0500

    rest-api - auth fix

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,9 +3,21 @@ from .models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            place=validated_data['place'],
+            biography=validated_data['biography'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'first_name', 'last_name', 'place', 'biography']
+        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'place', 'biography')
 
 
 class FollowersSerializer(serializers.ModelSerializer):
